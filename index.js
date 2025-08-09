411c46704a5b70cd2c0bce1b5558cf75a780288b
Rupon100 <ruponmia97@gmail.com>
2025-08-09 19:56:32 +0600

    all file setup

--- index.js
+++ index.js
@@ -7,9 +7,60 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Server is ok!")
-})
+require('dotenv').config()// for the doteenv
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nnefkr8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // collection
+
+    // user post
+
+    // user delete (same time delete from firebase)
+
+    // user personal account delete
+
+    // job get || use axios & tanstack query || design using framer motion later 
+
+    // job post
+
+    // job details
+
+    // job delete
+
+    // job patch
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
     console.log(`server is running on: ${port}`);
